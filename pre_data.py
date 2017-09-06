746c2b4a287ff7b11e2c5ed8be935bc1a1682415
Lukeeeeee <lukedong123@gmail.com>
2017-09-06 16:20:08 +0800

    modify data

--- pre_data.py
+++ pre_data.py
@@ -6,12 +6,42 @@ import numpy as np
 from glob import glob
 from PIL import Image
 from dataset import PATH
+'''
+{'num_cases_per_batch': 10000, 
+'label_names': ['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck'], 'num_vis': 3072}
+
+'''
+cifar10_dict = {
+'num_cases_per_batch': 10000,
+'label_names': ['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck'],
+'num_vis': 3072
+}
+
+def unpickle(file):
+    import cPickle
+    with open(file, 'rb') as fo:
+        dict = cPickle.load(fo)
+    return dict
 
 
 def get_datalist(data_dir, data_pattern):
     data_dir = PATH + data_dir
     image_list = glob(os.path.join(data_dir, data_pattern))
     return (image_list)
+
+
+def get_cifar10(data_dir, name):
+    data_dir = PATH + data_dir
+    list = cifar10_dict['label_names']
+    id = i
+    for i in range(len(list)):
+        if name == list[i]:
+            id = i
+    for i in range(1, 6):
+        data = unpickle(data_dir + '/data_batch_' + str(i))
+        pass
+    pass
+
 
 
 def get_image(image_list, batch_size, img_h, img_w):
@@ -33,3 +63,7 @@ def restruct_image(x, batch_size):
         # data = np.clip(data,0,255).astype(np.uint8)
         image_batch.append(data)
     return (image_batch)
+
+
+if __name__ == '__main__':
+    a = get_cifar10(data_dir='/cifar10/', name='cat')
